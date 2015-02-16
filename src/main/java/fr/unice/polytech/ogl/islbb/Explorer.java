32e14c5f712b5c852739e81b7e7db8d89245c406
Thibault Ober <thibault.ober@gmail.com>
2015-02-16 15:20:06 +0100

    Première version (quitter l'île, plus de throw exception).

--- src/main/java/fr/unice/polytech/ogl/islbb/Explorer.java
+++ src/main/java/fr/unice/polytech/ogl/islbb/Explorer.java
@@ -3,19 +3,26 @@ package fr.unice.polytech.ogl.islbb;
 import eu.ace_design.island.bot.*;
 
 public class Explorer implements IExplorerRaid {
-
+	/*
+	initialise notre explorateur
+	 */
     @Override
 	public void initialize(String context) {
-		throw new UnsupportedOperationException("IExplorerRaid::initialize is not yet implemented");
+	//ne fait rien
 	}
-
+	/*
+	Décide de la  marche à suivre
+	 */
     @Override
 	public String takeDecision() {
-		throw new UnsupportedOperationException("IExplorerRaid::takeDecision is not yet implemented");
+		String fin="{ \"action\": \"stop\" }";
+		return fin;
 	}
-
+	/*
+	traite les informations retournées par l'action en cours
+	 */
     @Override
 	public void acknowledgeResults(String results) {
-		throw new UnsupportedOperationException("IExplorerRaid::acknowledgeResults is not yet implemented");
+		//ne fait rien
 	}
 }
