9d7c5fb61d73d9d2b6a95e168f10813697438ee9
Lucas Sauvage <sl200103@etu.unice.fr>
2015-03-09 16:24:03 +0100

    Début de la classe ExitTest, classe de test de la classe Exit ISLBB-30

--- src/test/java/fr/unice/polytech/ogl/islbb/ExitTest.java
+++ src/test/java/fr/unice/polytech/ogl/islbb/ExitTest.java
@@ -1,19 +1,18 @@
 package fr.unice.polytech.ogl.islbb;
-import fr.unice.polytech.ogl.islbb.Exit;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  * Created by Lucas on 09/03/2015.
- * Classe de test pour la classe exit
+ * Classe de test pour la classe Exit
  */
 public class ExitTest  {
 
     @Test public void testExit() {
         Exit test = new Exit();
         Exit test2 = new Exit();
-        assertEquals(test, test2);
+        assertEquals(test.exit(), test2.exit());
 
     }
 }
