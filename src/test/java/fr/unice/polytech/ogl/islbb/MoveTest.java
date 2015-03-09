89f174827944a853eabc45112cb147742e28e232
Lucas Sauvage <sl200103@etu.unice.fr>
2015-03-09 16:25:17 +0100

    Début de la classe MoveTest, classe de test de la classe Move ISLBB-33

---
+++ src/test/java/fr/unice/polytech/ogl/islbb/MoveTest.java
@@ -0,0 +1,20 @@
+package fr.unice.polytech.ogl.islbb;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lucas on 09/03/2015.
+ * Classe de test pour la classe Move
+ */
+public class MoveTest {
+
+
+    @Test public void testLand() {
+        Move test = new Move();
+        Move test2 = new Move();
+        assertEquals(test.move("N"), test2.move("N"));
+
+    }
+
+}
