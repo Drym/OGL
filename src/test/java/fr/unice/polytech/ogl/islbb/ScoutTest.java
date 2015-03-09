2cd8ce47abba96c0b5730219a8e9d8f83ed77744
Lucas Sauvage <sl200103@etu.unice.fr>
2015-03-09 16:25:41 +0100

    Début de la classe ScoutTest, classe de test de la classe Scout ISLBB-34

--- src/test/java/fr/unice/polytech/ogl/islbb/ScoutTest.java
+++ src/test/java/fr/unice/polytech/ogl/islbb/ScoutTest.java
@@ -1,9 +1,20 @@
 package fr.unice.polytech.ogl.islbb;
 
-import fr.unice.polytech.ogl.islbb.*;
+        import org.junit.Test;
+        import static org.junit.Assert.*;
 
 /**
- * Created by QWPHR on 09/03/2015.
+ * Created by Lucas on 09/03/2015.
+ * Classe de test pour la classe Scout
  */
 public class ScoutTest {
+
+
+    @Test public void testScout() {
+        Scout test = new Scout();
+        Scout test2 = new Scout();
+        assertEquals(test.scout("N"), test2.scout("N"));
+
+    }
+
 }
