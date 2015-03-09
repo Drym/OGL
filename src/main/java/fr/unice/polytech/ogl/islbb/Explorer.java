8e2663d01126b6ae05f8d39ef7203597d98b30ca
Thibault Ober <thibault.ober@gmail.com>
2015-03-09 15:47:35 +0100

    création de la méthode analysant la Hashmap dans comportement. Réglages Explorer ISLBB-42

--- src/main/java/fr/unice/polytech/ogl/islbb/Explorer.java
+++ src/main/java/fr/unice/polytech/ogl/islbb/Explorer.java
@@ -16,54 +16,59 @@ public class Explorer implements IExplorerRaid {
 
     /**
      * Initialize the explorer with the given objectives.
+     *
      * @param context Objectives given by the arena.
      */
     @Override
     public void initialize(String context) {
-        data=new Data();
-        hascout=false;
-        comport=new Comportement();
-        card=0;
+        data = new Data();
+        decision = 0;
+        hascout = false;
+        comport = new Comportement();
+        card = 0;
         this.explorerInitialization = new Init(context);
         this.decision = 0;
     }
 
     /**
      * The AI of the explorer, makes his decisions.
+     *
      * @return Decision taken (JSON format).
      */
     @Override
     public String takeDecision() {
-        if (card>4){
-            hascout=true;
-            card=0;
-        }
-        while(!hascout){
-            card++;
-            Scout.scout(data.getCardinaux(card - 1));
-        }
         if (this.decision == 0) {
             this.decision++;
             return Land.land(this.explorerInitialization.getCreek(), 1).toString();
         }
-        else if (this.decision == 1) {
+        if (card > 4) {
+            hascout = true;
+            card = 0;
+        }
+        if (!hascout) {
+            card++;
+            return Scout.scout(data.getCardinaux(card - 1)).toString();
+        }
+        if (this.decision == 1) {
             this.decision++;
             return Scout.scout("N").toString();
         }
-        this.decision = 0;
         return Exit.exit().toString();
     }
 
     /**
      * Get the results returned from the arena, regarding the last decision executed.
+     *
      * @param results Results given by the arena.
      */
     @Override
     public void acknowledgeResults(String results) {
-        while(!hascout){
-            comport.setObj(results);
-            comport.getscout(data.getCardinaux(card-1));
+        if (this.decision != 0) {
+            while (!hascout) {
+                comport.setObj(results);
+                comport.getscout(data.getCardinaux(card - 1));
+            }
         }
-    }
 
+    }
 }
