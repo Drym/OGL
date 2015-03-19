3c4e9edfb30b1ce4489982ab717130176b2e8fb0
Thibault OBER <thibault.ober@gmail.com>
2015-03-19 14:11:20 +0100

    Création de Explorer test et test la première décision. ISLBB-58.

--- src/test/java/fr/unice/polytech/ogl/islbb/ExplorerTest.java
+++ src/test/java/fr/unice/polytech/ogl/islbb/ExplorerTest.java
@@ -1,14 +1,20 @@
 package fr.unice.polytech.ogl.islbb;
 
+import fr.unice.polytech.ogl.islbb.actions.Land;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
  * Created by user on 14/03/2015.
  */
 public class ExplorerTest {
-
+    String init="{\"creek\":\"creek_id\", \"budget\":600,\"men\":50,\"objective\":[{ \"resource\": \"WOOD\", \"amount\":600}]}";
     @Test public void testExplorer(){
-
-    }
+            Explorer test=new Explorer();
+            test.initialize(init);
+            String decision1=test.takeDecision();
+            assertEquals(decision1, Land.land("creek_id", 1));
+        }
 }
