7e59fc52cf751b6bf59ea9bede8a5c772b4ccd93
Quentin WEPHRE <quentin.wephre@hotmail.fr>
2015-03-19 15:27:14 +0100

    Ajout des informations de Biome dans les cases de la carte. ISLBB-63 ISLBB-64 ISLBB-66 ISLBB-67

---
+++ src/main/java/fr/unice/polytech/ogl/islbb/reports/Biome.java
@@ -0,0 +1,47 @@
+package fr.unice.polytech.ogl.islbb.reports;
+
+/**
+ * Created by QWPHR on 19/03/2015.
+ */
+public class Biome {
+
+    /**
+     * La chaîne de caractère décrivant le biome :
+     * Possibilités :
+     *  - OCEAN
+     *  - LAKE
+     *  - SHRUBLAND
+     *  - TROPICAL_SEASONAL_FOREST
+     *  - TROPICAL_RAIN_FOREST
+     *  - BEACH
+     *  - MANGROVE
+     */
+    private String biome;
+    private float percentage;
+
+    public Biome(String biomeName) {
+        this.biome = biomeName;
+    }
+
+    public Biome(String biomeName, float biomePercentage) {
+        this(biomeName);
+        this.percentage = biomePercentage;
+    }
+
+    public String getBiome() {
+        return biome;
+    }
+
+    public void setBiome(String biome) {
+        this.biome = biome;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+}
