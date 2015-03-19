0d07896bb777b52cedc7288eb8650b699b5d0663
Lucas Sauvage <sl200103@etu.unice.fr>
2015-03-19 14:52:32 +0100

    ISLBB-60 ISLBB-65
    Commande Glimpse traiter
    Test de la commande effectué ( vérifie le Json retourné)

---
+++ src/main/java/fr/unice/polytech/ogl/islbb/actions/Glimpse.java
@@ -0,0 +1,27 @@
+package fr.unice.polytech.ogl.islbb.actions;
+
+import org.json.JSONObject;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by Lucas on 19/03/2015.
+ * Classe permetant d'automatiser l'action glimpse pour l'explorer.
+ */
+public class Glimpse {
+
+    public static String glimpse(String direction, int range){
+
+        JSONObject obj = new JSONObject();
+        //TODO Demander a Mosser, si l'ordre est important
+        JSONObject param = new JSONObject();
+
+        param.put("direction",direction);
+        param.put("range",range);
+
+        obj.put("action","glimpse");
+        obj.put("parameters",param);
+
+        return obj.toString();
+
+    }
+}
