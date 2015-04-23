8f05663af7e650c26699fdd06a682f1bea2690ec
Lucas Sauvage <sl200103@etu.unice.fr>
2015-04-23 15:13:42 +0200

    ISLBB-124 Test ResultsComputing done v2

--- src/test/java/fr/unice/polytech/ogl/islbb/ResultsComputingTest.java
+++ src/test/java/fr/unice/polytech/ogl/islbb/ResultsComputingTest.java
@@ -18,10 +18,29 @@ public class ResultsComputingTest {
 
         assertEquals(test.getStatus("{status : OK}"), isTrue);
 
+        assertEquals(test.xOffset(0),0);
         assertEquals(test.xOffset(1),1);
+        assertEquals(test.xOffset(2),0);
+        assertEquals(test.xOffset(3),-1);
+        assertEquals(test.xOffset(10),0);
+
+        assertEquals(test.yOffset(0), 1);
         assertEquals(test.yOffset(1), 0);
+        assertEquals(test.yOffset(2), -1);
+        assertEquals(test.yOffset(3), 0);
+        assertEquals(test.yOffset(10), 0);
+
         assertEquals(test.parseAmount("LOW"), 0);
+        assertEquals(test.parseAmount("MEDIUM"), 1);
+        assertEquals(test.parseAmount("HIGH"), 2);
+        assertEquals(test.parseAmount("else"), -1);
+
         assertEquals(test.parseCondition("HARSH"), 0);
+        assertEquals(test.parseCondition("FAIR"), 1);
+        assertEquals(test.parseCondition("EASY"), 2);
+        assertEquals(test.parseCondition("else"), -1);
+
+        assertEquals(test.computeDistance(1,1,1,1), 0);
 
     }
 }
